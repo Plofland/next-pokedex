b311206fd6e4c5b134264674f18062af1607b64a
Plofland <ploflan@gmail.com>
2021-05-31 17:18:15 -0400

    API set up and pokemon set to state

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { Children } from 'react';
+import Head from 'next/Head';
 
-export default function Layout() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default function Layout({ title, children }) {
+	return (
+		<div className="bg-gray-300">
+			<main className="container mx-auto max-w-xl pt-8 min-h-screen">
+				{children}
+			</main>
+		</div>
+	);
 }
