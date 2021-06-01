0d6020678c4ef5b9ffdf6a205170caf01680eff3
Plofland <ploflan@gmail.com>
2021-05-31 20:16:00 -0400

    Pokedex completed and functional

--- pages/pokemon.js
+++ pages/pokemon.js
@@ -4,7 +4,39 @@ import Link from 'next/Link';
 
 export default function pokemon({ pokeman }) {
 	console.log(pokeman);
-	return <div></div>;
+	return (
+		<Layout title={pokeman.name}>
+			<h1 className="text-4xl mb-2 text-center capitalize">
+				{pokeman.name}
+			</h1>
+			<img
+				src={pokeman.image}
+				alt={pokeman.name}
+				className="mx-auto"
+			/>
+			<p>
+				<span className="font-bold mr-2">
+					Weight: {pokeman.weight}
+				</span>
+			</p>
+			<p>
+				<span className="font-bold mr-2">
+					Height: {pokeman.height}
+				</span>
+			</p>
+			<h2 className="text-2xl mt-6 mb-2">Types: </h2>
+			{pokeman.types.map((type, index) => (
+				<p key={index}>{type.type.name}</p>
+			))}
+			<p className="mt-10 text-center">
+				<Link href="/">
+					<a className="text-2xl underline">
+						Home
+					</a>
+				</Link>
+			</p>
+		</Layout>
+	);
 }
 
 //This is the other function that is the server-side rendered page (as opposed to a static page) that was mentioned in the index.js file
